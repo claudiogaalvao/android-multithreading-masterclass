0e089353a5e68bdb2f12889994087555c23d1f9c
claudiogaalvao <claudiogaalvao@gmail.com>
2023-04-17 21:32:18 -0300

    Third resolution

--- app/src/main/java/com/techyourchance/multithreading/exercises/exercise3/Exercise3Fragment.java
+++ app/src/main/java/com/techyourchance/multithreading/exercises/exercise3/Exercise3Fragment.java
@@ -1,6 +1,8 @@
 package com.techyourchance.multithreading.exercises.exercise3;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +26,8 @@ public class Exercise3Fragment extends BaseFragment {
 
     private Button mBtnCountSeconds;
     private TextView mTxtCount;
+
+    private final Handler mUiHandler = new Handler(Looper.getMainLooper());
 
     @Nullable
     @Override
@@ -55,5 +59,32 @@ public class Exercise3Fragment extends BaseFragment {
         3. Show count in TextView
         4. When count completes, show "done" in TextView and enable the button
          */
+        mBtnCountSeconds.setEnabled(false);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= SECONDS_TO_COUNT; i++) {
+                    final int count = i;
+                    mUiHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTxtCount.setText(count + "/" + SECONDS_TO_COUNT);
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                }
+                mUiHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTxtCount.setText("Done!");
+                        mBtnCountSeconds.setEnabled(true);
+                    }
+                });
+            }
+        }).start();
     }
 }
